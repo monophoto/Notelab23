db5647401982b6dd2c0bf4fe9e7236a637c36674
monophoto <jetsadawut1013@gmail.com>
2023-03-03 15:28:25 +0700

    average_comp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<cstdlib>
 
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+    float sum = 0;
+    int average = 0;
+    if(argc <= 1){
+        cout << "Please input numbers to find average.\n" ;
+    }else{
+        cout << "---------------------------------\n";
+        for(int i=0; i < argc; i++){
+            sum += atof(argv[i]);
+        }
+
+        average = sum/(argc-1);
+        cout << "Average of " << argc-1 << " numbers = " << average;
+        cout << "\n---------------------------------";
+        
+    }
+}
